15d0cbd8321967ccf25318509e544f11db499f03
Tirthankar231 <tirthankar@wemotiveforge.com>
2024-04-25 18:01:13 +0530

    ADD : Added the populate-data route

---
+++ src/api/populate-data/routes/populate-data.js
@@ -0,0 +1,10 @@
+module.exports = {
+    routes: [
+      {
+        method: 'POST',
+        path: '/populate-data',
+        handler: 'populate-data.create', // Adjust the controller and action name as needed
+      },
+    ]
+  }
+  
